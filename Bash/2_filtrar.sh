5c955ed0992c2c9a96bb323a5e94348d5ec6506f
guguidfr <guguidfr@protonmail.com>
2023-11-18 18:12:47 +0100

    Renombre de archivos

---
+++ Bash/2_filtrar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# José Daniel Fernández López
+
+<<'com'
+
+    Una manera sencilla de entender cómo funcionan los comandos 'cat', 'head', 'tail, 'grep', 'awk', y las 'pipes' [|] es usando el archivo 'passwd'.
+    Ahora, te explicaré el funcionamiento de estos:
+    - 'cat':
+
+com
