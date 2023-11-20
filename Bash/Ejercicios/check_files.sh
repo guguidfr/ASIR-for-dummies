8b0bf8185c9443382ebf27dc415ba86f06279437
guguidfr <guguidfr@protonmail.com>
2023-11-20 16:55:53 +0100

    Archivo creado

---
+++ Bash/Ejercicios/check_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# José Daniel Fernández López
+
+<<'com'
+
+    En linux, usando 'if', podemos verificar archivos, directorios y permisos.
+    Esto nos puede resultar útil si tenemos una máquina de la que no conocemos el árbol de directorios,
+    o queremos hacer que comprobar algo en el sistema sea maś cómodo que usar comandos.
+
+    Para esto, tenemos diferentes opciones para poner en las condiciones del 'if':
+    
+
+com
