60fdf1ad9a81dd6add13c01226e867b891d0898d
guguidfr <guguidfr@protonmail.com>
2024-01-18 16:53:30 +0100

    Introducción bucles

---
+++ Bash/4_bucles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# José Daniel Fernández López
+
+<<'com'
+
+    A la hora de crear scripts, nos encontraremos situaciones en las que tendremos que repetir un comando o una serie de pasos más de una vez.
+    Para evitar la repetición de código contamos con lo que llamamos "bucles".
+    En bash tenemos 2 bucles: "for" y "while".
+    Con "for" podemos hacer un número determinado de repeticiones, por lo que siempre tendremos el control, lo que evitará que hagamos bucles infinitos o se hagan más repeticiones de las necesarias.
+    Con "while" podemos crear tanto bucles "infinitos" como bucles con repeticiones concretas (aunque no sea su uso principal).
+    El operador "while" se usa cuando queremos repetir algo mientras una condición se cumpla o hagamos que la salida sea forzosa. Es útil cuando queremos un programa interactivo con el usuario.
+
+    Además, cuando necesitemos leer un archivo de texto línea a línea, usaremos "while" (porque normalmente no sabremos )
+
+com
+
